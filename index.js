47c41df04c0556ea02fb23cebfa42cb1e0214153
keldenl <keldenlin@gmail.com>
2022-08-20 17:20:14 -0700

    more setup

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 9000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`App listening on Port ${port}`);
 });
